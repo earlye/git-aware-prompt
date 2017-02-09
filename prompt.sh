2a72cc34dc16192be9cd53a9e6d7be7d0731b1b6
Early Ehlinger <earlye@gmail.com>
2017-02-09 08:27:33 -0600

    An attempt to support a file in the root of the git repo, called .git_prompt_nodirty, whose presence causes the dirty flag to not show up in the prompt.

--- prompt.sh
+++ prompt.sh
@@ -1,29 +1,37 @@
 find_git_branch() {
-  # Based on: http://stackoverflow.com/a/13003854/170413
-  local branch
-  if branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null); then
-    if [[ "$branch" == "HEAD" ]]; then
-      branch='detached*'
+    # Based on: http://stackoverflow.com/a/13003854/170413
+
+    local branch # bash for "declare local variable named 'branch'"
+    
+    branch=$(git branch-name)
+    if [[ ! -z $branch ]] ; then
+        if [[ "$branch" == "HEAD" ]]; then
+            branch='(*detached)'
+        fi
+        git_branch="($branch)"
+    else
+        git_branch=""
     fi
-    git_branch="($branch)"
-  else
-    git_branch=""
-  fi
 }
 
 find_git_dirty() {
-  local status=$(git status --porcelain 2> /dev/null)
-  if [[ "$status" != "" ]]; then
-    git_dirty='*'
-  else
-    git_dirty=''
-  fi
+    GIT_ROOT=$(git rev-parse --show-toplevel 2> /dev/null )
+    if [[ ! -f $GIT_ROOT/.git_prompt_nodirty ]]; then
+        git_dirty=''
+    else
+        local status=$(git status --porcelain 2> /dev/null)
+        if [[ "$status" != "" ]]; then
+            git_dirty='*'
+        else
+            git_dirty=''
+        fi
+    fi
 }
 
 PROMPT_COMMAND="find_git_branch; find_git_dirty; $PROMPT_COMMAND"
 
 # Default Git enabled prompt with dirty state
-# export PS1="\u@\h \w \[$txtcyn\]\$git_branch\[$txtred\]\$git_dirty\[$txtrst\]\$ "
+export PS1="\u@\h \w \[$txtcyn\]\$git_branch\[$txtred\]\$git_dirty\[$txtrst\]\$ "
 
 # Another variant:
 # export PS1="\[$bldgrn\]\u@\h\[$txtrst\] \w \[$bldylw\]\$git_branch\[$txtcyn\]\$git_dirty\[$txtrst\]\$ "
